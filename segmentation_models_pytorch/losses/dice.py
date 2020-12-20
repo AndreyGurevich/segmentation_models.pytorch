a34767b2f0a0dc30b647620446a2db40470753c8
Andrey Gurevich <amber4eg@gmail.com>
2020-12-20 23:43:23 +0300

    Update dice.py
    add __name__ to DiceLoss

--- segmentation_models_pytorch/losses/dice.py
+++ segmentation_models_pytorch/losses/dice.py
@@ -10,6 +10,15 @@ __all__ = ["DiceLoss"]
 
 
 class DiceLoss(_Loss):
+    
+    @property
+    def __name__(self):
+        if self._name is None:
+            name = self.__class__.__name__
+            s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+            return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        else:
+            return self._name
 
     def __init__(
         self,
